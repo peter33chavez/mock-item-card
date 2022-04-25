b6faf4f91476e36b60e966c2a805e5c615082bd4
Peter Chavez <peter33chavez@gmail.com>
2022-04-24 20:02:03 -0700

    CSS constrains for bigger screens

--- src/components/FormatItemDetails/format-item-details.style.ts
+++ src/components/FormatItemDetails/format-item-details.style.ts
@@ -1,43 +1,42 @@
 import styled from "styled-components";
 
 export const ItemDetailsContainer = styled.div`
-    display: flex;
-    gap: 0 3rem;
-    padding: 3rem;
-    @media (max-width: 1170px) {
-        align-items: center;
-        flex-direction: column;
-    }
+  display: flex;
+  gap: 0 3rem;
+  padding: 3rem;
+  @media (max-width: 1170px) {
+    align-items: center;
+    flex-direction: column;
+  }
 `;
 export const ImageContainer = styled.div`
-     min-width: 350px;
-     width: 45vw;
-    img{
-        width: 100%;
-    }
+  min-width: 350px;
+  width: 60vw;
+  img {
+    width: 100%;
+  }
 `;
 export const Details = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 3rem;
-    max-width: 55vw;
-    @media (max-width: 1170px) {
-        margin-top: 2rem;   
-        max-width: 90vw;
-    }
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  max-width: 55vw;
+  @media (max-width: 1170px) {
+    margin-top: 2rem;
+    max-width: 90vw;
+  }
 `;
 export const Description = styled.div`
-    h1{
-        margin-bottom: 1rem;
-        text-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-    }
+  h1 {
+    margin-bottom: 1rem;
+    text-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+  }
 `;
 export const Variants = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    gap: 1rem 1rem;
-    @media (max-width: 1170px) {
-        justify-content: center ;
-    }
-
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem 1rem;
+  @media (max-width: 1170px) {
+    justify-content: center;
+  }
 `;
--- src/pages/ItemDetails/item-details.style.ts
+++ src/pages/ItemDetails/item-details.style.ts
@@ -1,15 +1,16 @@
 import styled from "styled-components";
 
 export const Loading = styled.div`
-    align-items: center;
-    display: flex;
-    padding-top: 40rem;
+  align-items: center;
+  display: flex;
+  padding-top: 40rem;
 `;
 export const ItemContainer = styled.div`
-    background: #F0F6FF;
-    font-family: var(--primary-font);
+  background: #f0f6ff;
+  font-family: var(--primary-font);
+  min-height: 100vh;
+  max-width: 2000px;
 `;
 export const Header = styled.div`
-    box-shadow: 0 0 5px rgba(0, 0, 0, .2);
-    
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
 `;
